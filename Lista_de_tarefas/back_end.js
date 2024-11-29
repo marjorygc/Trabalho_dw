8c0f6dabf294670eeba98db8ebca2ee4243bd3c0
10mara <163425297+10mara@users.noreply.github.com>
2024-11-29 17:36:13 -0300

    dia3.1

--- Lista_de_tarefas/back_end.js
+++ Lista_de_tarefas/back_end.js
@@ -8,6 +8,32 @@ plus.onclick = add_tarefa
 
 modoBotao.addEventListener("click", changebackground)
 
+
+document.addEventListener('keydown', function(e) {
+    if (e.ctrlKey && e.key.toLowerCase() === 'h') {
+        e.preventDefault();
+        console.log("ok")
+        const tela2 = document.querySelector("#tela");
+        const tela1 = document.querySelector("#container");
+        const tela2Display = window.getComputedStyle(tela2).display;
+        if (tela2Display === 'none') {
+            tela2.style.display = 'block';
+            tela1.style.display = 'none';
+        } else {
+            tela2.style.display = 'none';
+            tela1.style.display = 'block';
+        }
+        const closeIcon= document.querySelector("#closeIcon")
+        closeIcon.addEventListener("click",closeMenu)
+
+        function closeMenu() {
+            tela1.style.display = 'block';
+            tela2.style.display = 'none';
+            
+        }
+    }
+    });
+
 function changebackground(){
     if(body.style.backgroundImage == 'url("imagens/imagem-fundo.avif")'){
          body.style.backgroundImage = 'url("imagens/back_escuro.png")'
