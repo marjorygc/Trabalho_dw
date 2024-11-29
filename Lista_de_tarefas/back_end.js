a95a1d560af80780e3bee5d607d9d16770508784
marjorygc <163425550+marjorygc@users.noreply.github.com>
2024-11-29 09:53:14 -0300

    modo escuro/ teste

--- Lista_de_tarefas/back_end.js
+++ Lista_de_tarefas/back_end.js
@@ -1,8 +1,16 @@
 const modoBotao = document.querySelector("#modoBotao")
 const container = document.querySelector("#container")
+const botaoImg = document.querySelector("#botaoImg")
 
 modoBotao.addEventListener("click", changebackground)
 
 function changebackground(){
-    container.style.backgroundImage = url("https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.pngtree.com%2Ffree-backgrounds-photos%2Fgradiente-azul-petr%25C3%25B3leo-pictures&psig=AOvVaw0YulLU8FkWvcWULO-TL4Fz&ust=1732967090178000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLiajdi7gYoDFQAAAAAdAAAAABAE")
+    if(container.style.backgroundImage == 'url("imagens/imagem-fundo.avif")'){
+         container.style.backgroundImage = 'url("imagens/back_escuro.png")'
+         botaoImg.src = "https://img.icons8.com/?size=100&id=45474&format=png&color=000000"; 
+    } else {
+        container.style.backgroundImage = 'url("imagens/imagem-fundo.avif")'
+        botaoImg.src = "https://img.icons8.com/?size=100&id=648&format=png&color=000000"; 
+    }
+    
 }
